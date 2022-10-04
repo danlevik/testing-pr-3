c1b94e5828fa2315032a8889cb40132fe47316d2
danlevik <leviol80@gmail.com>
2022-10-04 13:34:29 +0300

    created basic tests

---
+++ main.py
@@ -0,0 +1,10 @@
+
+class Converter:
+    def __init__(self):
+        pass
+
+    def choose_unit_class(choice):
+        return None;
+
+    def choose_unit(choice):
+        return None;
---
+++ tests.py
@@ -0,0 +1,86 @@
+import main
+
+import unittest
+
+
+class TestChooseMethods(unittest.TestCase):
+
+    def test_choose_unit_class_should_return_right_unit_class(self):
+        ans = main.Converter.choose_unit_class(3);
+        self.assertEqual("Температура", ans);
+
+    def test_choose_unit_class_should_return_error_message(self):
+        ans = main.choose_unit_class(3);
+        self.assertEqual("none", ans);
+
+    #
+    #
+
+    def test_choose_unit_weight_should_return_right_unit(self):
+        ans = main.choose_unit_weight(3);
+        self.assertEqual("килограмм", ans);
+
+    def test_choose_unit_weight_should_return_error_message(self):
+        ans = main.choose_unit_weight(30);
+        self.assertEqual("none", ans);
+
+    #
+
+    def test_choose_unit_distance_should_return_right_unit(self):
+        ans = main.choose_unit_distance(3);
+        self.assertEqual("метр", ans);
+
+    def test_choose_unit_distance_should_return_error_message(self):
+        ans = main.choose_unit_distance(30);
+        self.assertEqual("none", ans);
+
+    #
+
+    def test_choose_unit_volume_should_return_right_unit(self):
+        ans = main.choose_unit_volume(3);
+        self.assertEqual("галлон", ans);
+
+    def test_choose_unit_volume_should_return_error_message(self):
+        ans = main.choose_unit_volume(30);
+        self.assertEqual("none", ans);
+
+    #
+
+    def test_choose_unit_area_should_return_right_unit(self):
+        ans = main.choose_unit_area(3);
+        self.assertEqual("гектар", ans);
+
+    def test_choose_unit_area_should_return_error_message(self):
+        ans = main.choose_unit_area(30);
+        self.assertEqual("none", ans);
+
+    #
+
+    def test_choose_unit_temperature_should_return_right_unit(self):
+        ans = main.choose_unit_temperature(3);
+        self.assertEqual("кельвин", ans);
+
+    def test_choose_unit_temperature_should_return_error_message(self):
+        ans = main.choose_unit_temperature(30);
+        self.assertEqual("none", ans);
+
+    #
+
+    def test_choose_unit_speed_should_return_right_unit(self):
+        ans = main.choose_unit_speed(3);
+        self.assertEqual("километр в час", ans);
+
+    def test_choose_unit_speed_should_return_error_message(self):
+        ans = main.choose_unit_speed(30);
+        self.assertEqual("none", ans);
+
+
+class TestConvertation(unittest.TestCase):
+
+    def test_convert_weight_to_default_value(self):
+        pass;
+
+    # ...
+
+    def test_convert_distance_to_miles(self):
+        pass;
