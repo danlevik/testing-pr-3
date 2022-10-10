2af3b1e67ce6b1e9265ca369fe982dd4c5fc0713
danlevik <leviol80@gmail.com>
2022-10-10 12:27:35 +0300

    rebuilt tests

--- tests.py
+++ tests.py
@@ -1,86 +1,42 @@
 import main
-
 import unittest
 
 
 class TestChooseMethods(unittest.TestCase):
 
     def test_choose_unit_class_should_return_right_unit_class(self):
-        ans = main.Converter.choose_unit_class(3)
-        self.assertEqual("Температура", ans)
+        conv = main.Converter()
+        ans = conv.choose_unit_class(2)
+        self.assertEqual("Объем", ans)
 
     def test_choose_unit_class_should_return_error_message(self):
-        ans = main.choose_unit_class(3)
-        self.assertEqual("none", ans)
+        conv = main.Converter()
+        ans = conv.choose_unit_class(8)
+        self.assertEqual("Wrong input", ans)
 
     #
     #
 
-    def test_choose_unit_weight_should_return_right_unit(self):
-        ans = main.choose_unit_weight(3)
-        self.assertEqual("килограмм", ans)
+    def test_choose_unit_should_return_right_unit_class(self):
+        conv = main.Converter()
+        ans = conv.choose_unit("Расстояние", 3)
+        self.assertEqual("миля", ans)
 
-    def test_choose_unit_weight_should_return_error_message(self):
-        ans = main.choose_unit_weight(30)
-        self.assertEqual("none", ans)
-
-    #
-
-    def test_choose_unit_distance_should_return_right_unit(self):
-        ans = main.choose_unit_distance(3)
-        self.assertEqual("метр", ans)
-
-    def test_choose_unit_distance_should_return_error_message(self):
-        ans = main.choose_unit_distance(30)
-        self.assertEqual("none", ans)
+    def test_choose_unit_should_return_error_message(self):
+        conv = main.Converter()
+        ans = conv.choose_unit("Площадь", 6)
+        self.assertEqual("Wrong input", ans)
 
     #
-
-    def test_choose_unit_volume_should_return_right_unit(self):
-        ans = main.choose_unit_volume(3)
-        self.assertEqual("галлон", ans)
-
-    def test_choose_unit_volume_should_return_error_message(self):
-        ans = main.choose_unit_volume(30)
-        self.assertEqual("none", ans)
-
     #
 
-    def test_choose_unit_area_should_return_right_unit(self):
-        ans = main.choose_unit_area(3)
-        self.assertEqual("гектар", ans)
+    def test_process_unit_should_return_correct_dict(self):
+        check_dict = {"килограмм": 2, "карат": 10000,
+                       "грамм": 2000, "тонна": 0.002,
+                       "фунт": 4.410, "унция": 70.54,
+                       "драхма": 1128.8, "гран": 30864,
+                       }
 
-    def test_choose_unit_area_should_return_error_message(self):
-        ans = main.choose_unit_area(30)
-        self.assertEqual("none", ans)
-
-    #
-
-    def test_choose_unit_temperature_should_return_right_unit(self):
-        ans = main.choose_unit_temperature(3)
-        self.assertEqual("кельвин", ans)
-
-    def test_choose_unit_temperature_should_return_error_message(self):
-        ans = main.choose_unit_temperature(30)
-        self.assertEqual("none", ans)
-
-    #
-
-    def test_choose_unit_speed_should_return_right_unit(self):
-        ans = main.choose_unit_speed(3)
-        self.assertEqual("километр в час", ans)
-
-    def test_choose_unit_speed_should_return_error_message(self):
-        ans = main.choose_unit_speed(30)
-        self.assertEqual("none", ans)
-
-
-class TestConvertation(unittest.TestCase):
-
-    def test_convert_weight_to_default_value(self):
-        pass
-
-    # ...
-
-    def test_convert_distance_to_miles(self):
-        pass
+        conv = main.Converter()
+        ans = conv.process_unit("Вес", "фунт", 4.410)
+        self.assertEqual(check_dict, ans)
